ed5627dd8ec2b834e09479d4a741e35b95ce0adb
Jia <cwu005104@gmail.com>
2023-05-20 17:05:54 +0800

    feat: add mongodb connect

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express')
 const line = require('@line/bot-sdk');
 const crypto = require('crypto')
+const mongodb = require('./config/mongoose')
+
 const app = express()
+
 const PORT = process.env.PORT
 const config = {
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
@@ -15,36 +18,44 @@ const config = {
 
 const client = new line.Client(config);
 
+// connect mongoose
+(async () => {
+  await mongodb.connect()
+})
+
+// set event
 function handleEvent(event) {
   const message = {
     type: 'text',
     text: 'Hello World!'
   }
-  //判斷發送過來的訊息種類
+  // determine receive event type
   if (event.type !== 'message' || event.message.type !== 'text') {
     message.text = '拍謝，看不懂';
   }
-  return client.replyMessage(event.replyToken, message);//回覆訊息
+
+  // reply message
+  return client.replyMessage(event.replyToken, message);
 }
 
-//設定webhook路由
+// set webhook route
 app.post('/webhook', line.middleware(config), (req, res) => {
   let signInKey = '';
   try {
-    //產生對照組header
+    // produce reference header
     signInKey = crypto.createHmac('sha256', config.channelSecret).
       update(Buffer.from(JSON.stringify(req.body)), 'utf8').digest('base64');
   } catch (e) {
-    //產生失敗的處理
+    // error handle
     console.log(e)
   }
 
-  //比對產生出的header是否與line官方的header相符，不符就回傳錯誤
+  // compare if reference header is the same as line official header, if not, return error
   if (signInKey !== req.header('x-Line-Signature')) {
     return res.send(error);
   }
-  return res.json(handleEvent(req.body.events[0]))
 
+  return res.json(handleEvent(req.body.events[0]))
 });
 
 app.listen(PORT, () => {
