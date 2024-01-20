f0cf4102b90a26c9bcf47bd41d9c84a6d1dcbe43
Jia <cwu005104@gmail.com>
2024-01-20 22:54:17 +0800

    feat: edit notes in input-data

--- helpers/input-data.js
+++ helpers/input-data.js
@@ -10,20 +10,21 @@ module.exports = {
             let questionIndex = record.questionIndex
             const replyToken = event.replyToken
             if (event.type === 'message') {
-                // image 處理
+                // image 處理 Deal with image
                 if (event.message.type == 'image') {
                     const imageId = event.message.id;
                     const downloadUrl = `https://api-data.line.me/v2/bot/message/${imageId}/content`;
                     return downloadImage(downloadUrl)
                         .then(async (imageData) => {
-                            // 轉換為 Base64
+                            // 轉換為 Base64 Transform to base64
                             const base64Data = imageData.toString('base64')
 
-                            // 上傳 imgur 並存取imgur url
+                            // 上傳 imgur 並存取imgur url upload base64 & save imgur url
                             const imgurl = await uploadImgur(base64Data)
+                            console.log(imgurl)
                             record.answer.image.push(imgurl)
 
-                            // 已完成回傳答覆
+                            // 已完成回傳答覆 Reply input completed
                             if (record.questionIndex >= questions.length) {
                                 record.questionIndex = 0
                                 record.status = status[0]
@@ -36,13 +37,13 @@ module.exports = {
                         });
                 }
             }
-            // 本日未完成跑問題
+            // 本日未完成跑問題 questions unfinished
             if (questionIndex < questions.length) {
 
-                // 向使用者提問
+                // 向使用者提問 ask users
                 await client.replyMessage(replyToken, { type: 'text', text: questions[questionIndex] });
 
-                // 第一次呼叫不存入資料庫
+                // 第一次呼叫不存入資料庫 No input first response
                 if (record.questionIndex !== 0 && event.type === 'message') {
                     record.answer.text.push(event.message.text)
                 }
@@ -52,11 +53,11 @@ module.exports = {
                 };
 
             } else if (record && record.status !== status[0]) {
-                // turn record status to false restrict editing
+                // 若問答結束且紀錄狀態為可編輯，狀態改為唯讀 turn record status to readonly restrict editing
                 if (record.questionIndex === questions.length) record.questionIndex = 0
                 record.status = status[0]
                 await record.save()
-                // 已完成回傳答覆
+                // 已完成回傳答覆 Reply input completed
                 await client.replyMessage(replyToken, { type: 'text', text: '所有問題已完成' })
             }
         } catch (err) { console.log(err) }
